8fb34ae6b6a334e5d53967f050dcf20cb93a00df
eafit-201710002010 <imurielr@eafit.edu.co>
2018-05-16 21:48:28 -0500

    Código final

--- proyecto/codigo/DigraphAM.java
+++ proyecto/codigo/DigraphAM.java
@@ -1,30 +1,45 @@
 import java.util.ArrayList;
+import javafx.util.Pair;
 
 /**
  * Implementacion de un grafo dirigido usando matrices de adyacencia
  */
 public class DigraphAM extends Digraph {
-	int[][] matrix;
 
-	public DigraphAM(int size) {
-		super(size);
-		matrix = new int[size][size];
-	}
-
-	public void addArc(int source, int destination, int weight) {
-		matrix[source][destination] = weight;
-	}
-
-	public ArrayList<Integer> getSuccessors(int vertex) {
-		ArrayList<Integer> s = new ArrayList<>();
-		for (int i = 0; i < size; ++i)
-			if (matrix[vertex][i] != 0)
-				s.add(i);
-		return s.size() == 0 ? null : s;
-	}
-
-	public int getWeight(int source, int destination) {
-		return matrix[source][destination];
-	}
-
+  int[][] matriz;
+  
+  /**
+   * Constructor de un grafo utilizando matrices.
+   */
+  public DigraphAM(int size) {
+    super(size);
+    matriz = new int[size][size];
+  }
+  
+  /**
+   * Método para cálcular la distancia que hay de un vertice a otro.
+   */
+  public int getWeight(int source, int destination) {
+    return matriz[source][destination];
+  }
+  
+  /**
+   * Método para añadir un arco al grafo.
+   */
+  public void addArc(int source, int destination, int weight) {
+    matriz[source][destination] = weight;
+  }
+  
+  /**
+   * Método para obtener los vertices a los que se puede llegar a partir del vertice actual.
+   */
+  public ArrayList<Integer> getSuccessors(int vertex) {
+    ArrayList<Integer> lista = new ArrayList<Integer>();
+    for (int j = 0; j < size; j++) {
+      if (matriz[vertex][j] != 0) {
+        lista.add(j);
+      }
+    }
+    return lista;
+  }
 }
---
+++ proyecto/codigo/Estacion.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Estacion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Estacion
+{
+    int tipo;
+    float tiempoCarga;
+    /**
+     * Constructor de estación
+     * @param tipo El parámetro tipo representa el tipo de estacion, que puede ser 0, 1 o 2
+     * @param tiempoCarga El parámetro tiempoCarga representa el tiempo en horas que se demora la estación
+     * para cargar un camión completamente.
+     */
+    public Estacion(int tipo, float tiempoCarga){
+        this.tipo = tipo;
+        this.tiempoCarga = tiempoCarga;
+    }
+}
